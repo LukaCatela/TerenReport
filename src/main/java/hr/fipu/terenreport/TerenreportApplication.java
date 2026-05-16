792cb3a7b24e909034c9cc22ff300c8443433a7e
Luka Catela <luca.catela8@gmail.com>
2026-05-16 22:51:48 +0200

    Prvi commit

---
+++ src/main/java/hr/fipu/terenreport/TerenreportApplication.java
@@ -0,0 +1,13 @@
+package hr.fipu.terenreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerenreportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerenreportApplication.class, args);
+    }
+
+}
